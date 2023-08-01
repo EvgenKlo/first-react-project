2e6072c2a4619c9305cb36af4d14d4a0fc049792
EvgeKlo <klokovevgen@gmail.com>
2023-08-02 01:17:05 +0500

    feat: 40:12

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import React, {useState} from "react";
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+import './styles/app.css'
+import PostItem from "./components/PostItem";
 
 function App() {
-  const [likes, setLikes] = useState(0);
-  const [value, setValue] = useState('Input text')
-  
-  function increment() {
-    setLikes(likes + 1)
-  }
-
-  function decrement() {
-    setLikes(likes - 1)
-  }
+  const [posts, setState] = useState([
+    {id: 1, title: 'Javascript', body: 'Description'},
+    {id: 2, title: 'Javascript 1', body: 'Description'},
+    {id: 3, title: 'Javascript 2', body: 'Description'}
+  ])
 
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
-      <input
-        type="text"
-        value={value}
-        onChange={event => setValue(event.target.value)}
-      />
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <h1 style={{textAlign: "center"}}>Posts list</h1>
+      {posts.map(post =>
+        <PostItem post={post} key={post.id}/>
+        )}
     </div>
   );
 }
