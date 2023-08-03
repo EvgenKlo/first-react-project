f867d685f2693264c99c3cebb0b877eeb6c6afce
EvgenKlo <klokovevgen@gmail.com>
2023-08-04 02:52:07 +0500

    feat: 2:16:26

--- src/App.js
+++ src/App.js
@@ -1,69 +1,26 @@
-import React, {useState} from "react";
+import React from "react";
 import './styles/app.css'
-import PostList from "./components/PostList";
-import PostForm from "./components/PostForm";
-import MySelect from "./components/UI/select/MySelect";
-import MyInput from "./components/UI/input/MyInput";
+import { BrowserRouter, Route, Routes, RouterProvider, createBrowserRouter } from "react-router-dom";
+import About from "./pages/About";
+import Posts from "./pages/Posts";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {id: 1, title: 'aa', body: 'ff'},
-    {id: 2, title: 'bb', body: 'ee'},
-    {id: 3, title: 'cc', body: 'dd'}
-  ])
-
-  const [selectedSort, setSelectedSort] = useState('');
-
-  const [searchQuery, setSearchQuery] = useState('');
-
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost])
-  }
-
-  const removePost = (post) => {
-    setPosts(posts.filter(p => p.id !== post.id))
-  }
-
-  const sortPosts = (sort) => {
-    setSelectedSort(sort);
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
-  }
-
   return (
-    <div className="App">
-      <PostForm create={createPost}/>
-      <hr style={{margin: '15px 0'}}/>
-      <MyInput
-        value={searchQuery}
-        onChange={e => setSearchQuery(e.target.value)}
-        placeholder='Search...'
-      />
-      <div>
-        <MySelect
-          value={selectedSort}
-          onChange={sortPosts}
-          defaultValue={'Sorting '}
-          options={[
-            {value: 'title', name: 'by name'},
-            {value: 'body', name: 'by description'},
-          ]}
-        />
-      </div>
-      {posts.length
-        ?
-        <PostList
-          remove={removePost}
-          posts={posts}
-          title='Posts about JS'
-        />
-        :
-        <h1 style={{textAlign: 'center'}}>
-          Posts not found
-        </h1>
-      }
-      
-    </div>
-  );
+    <RouterProvider>
+      <Routes>
+        <Route
+          path="/about"
+          element={<About/>}
+        >
+        </Route>
+        <Route
+          path="/posts"
+          element={<Posts/>}
+        >
+        </Route>
+      </Routes>
+    </RouterProvider>
+  )
 }
 
 export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <h1>
+      Application is working
+    </h1>
+  )
+}
+
+export default About
